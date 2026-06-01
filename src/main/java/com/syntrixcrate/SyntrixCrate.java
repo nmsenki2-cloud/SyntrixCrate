7d83cd6213a2fd3b6ed643c683d6089412dbeefc
nmsenki2-cloud <nmsenki2@gmail.com>
2026-06-01 21:01:33 +0200

    Update SyntrixCrate.java

--- src/main/java/com/syntrixcrate/SyntrixCrate.java
+++ src/main/java/com/syntrixcrate/SyntrixCrate.java
@@ -1,10 +1,12 @@
 package com.syntrixcrate;
 
+import com.syntrixcrate.animation.AnimationManager;
 import com.syntrixcrate.commands.CratesCommand;
+import com.syntrixcrate.hologram.HologramManager;
 import com.syntrixcrate.listeners.CrateListener;
 import com.syntrixcrate.managers.CrateManager;
-import com.syntrixcrate.managers.KeyManager;
 import com.syntrixcrate.managers.CooldownManager;
+import com.syntrixcrate.managers.KeyManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class SyntrixCrate extends JavaPlugin {
@@ -13,6 +15,8 @@ public class SyntrixCrate extends JavaPlugin {
     private CrateManager crateManager;
     private KeyManager keyManager;
     private CooldownManager cooldownManager;
+    private HologramManager hologramManager;
+    private AnimationManager animationManager;
     private CrateListener crateListener;
 
     @Override
@@ -20,9 +24,11 @@ public class SyntrixCrate extends JavaPlugin {
         instance = this;
         saveDefaultConfig();
 
-        this.crateManager    = new CrateManager(this);
-        this.keyManager      = new KeyManager(this);
-        this.cooldownManager = new CooldownManager(this);
+        this.crateManager     = new CrateManager(this);
+        this.keyManager       = new KeyManager(this);
+        this.cooldownManager  = new CooldownManager(this);
+        this.hologramManager  = new HologramManager(this);
+        this.animationManager = new AnimationManager(this);
 
         this.crateListener = new CrateListener(this);
         getServer().getPluginManager().registerEvents(crateListener, this);
@@ -31,20 +37,27 @@ public class SyntrixCrate extends JavaPlugin {
         getCommand("crates").setExecutor(cmd);
         getCommand("crates").setTabCompleter(cmd);
 
+        hologramManager.loadAll();
+        animationManager.loadAll();
+
         getLogger().info("SyntrixCrate betöltve! Ládák: " + crateManager.getAllCrateTypes().size());
     }
 
     @Override
     public void onDisable() {
         crateManager.saveLocations();
+        hologramManager.removeAll();
+        animationManager.stopAll();
         getLogger().info("SyntrixCrate leállt.");
     }
 
-    public static SyntrixCrate getInstance()        { return instance; }
-    public CrateManager getCrateManager()           { return crateManager; }
-    public KeyManager getKeyManager()               { return keyManager; }
-    public CooldownManager getCooldownManager()     { return cooldownManager; }
-    public CrateListener getCrateListener()         { return crateListener; }
+    public static SyntrixCrate getInstance()       { return instance; }
+    public CrateManager getCrateManager()          { return crateManager; }
+    public KeyManager getKeyManager()              { return keyManager; }
+    public CooldownManager getCooldownManager()    { return cooldownManager; }
+    public HologramManager getHologramManager()    { return hologramManager; }
+    public AnimationManager getAnimationManager()  { return animationManager; }
+    public CrateListener getCrateListener()        { return crateListener; }
 
     public String msg(String path) {
         String prefix = getConfig().getString("messages.prefix", "&8[&6SyntrixCrate&8] &r");
